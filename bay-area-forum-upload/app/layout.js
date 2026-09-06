1832acf99cf40f50ac5ee209f87391374de29164
XiaoHui <iaoui730@gmail.com>
2026-09-06 00:01:47 -0700

    Update layout.js

--- bay-area-forum-upload/app/layout.js
+++ bay-area-forum-upload/app/layout.js
@@ -1,3 +1,3 @@
 import './globals.css';
-export const metadata={title:'湾区论坛',description:'匿名湾区论坛'};
+export const metadata={title:'匿名的熟人论坛',description:'匿名的熟人论坛'};
 export default function RootLayout({children}){return <html lang="zh-CN"><body>{children}</body></html>}
